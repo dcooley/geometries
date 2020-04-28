73915410328cc205eadd2b7b7db2147e7a6f5f58
David <david.cooley141@gmail.com>
2020-04-28 20:14:24 +1000

    utils for #3

--- inst/include/geometries/utils/utils.hpp
+++ inst/include/geometries/utils/utils.hpp
@@ -45,43 +45,6 @@ namespace utils {
     }
   }
 
-  // inline bool is_null_geometry( Rcpp::IntegerVector& iv, std::string geom_type ) {
-  //   int n = iv.length();
-  //   if( geom_type == "POINT" ) {
-  //     if ( iv[0] == NA_INTEGER || iv[1] == NA_INTEGER ) {
-  //       return true;
-  //     }
-  //   } else if ( n == 0 ) { // #nocov
-  //     return true;         // #nocov
-  //   }
-  //   return false;
-  // }
-  //
-  // inline bool is_null_geometry( Rcpp::NumericVector& nv, std::string geom_type ) {
-  //   int n = nv.length();
-  //   if( geom_type == "POINT" ) {
-  //
-  //     if (ISNAN( nv[0] ) || ISNAN( nv[1] ) ) {
-  //       return true;
-  //     }
-  //   } else if ( n == 0 ) { // #nocov
-  //     return true;         // #nocov
-  //   }
-  //   return false;
-  // }
-  //
-  // inline bool is_null_geometry( SEXP& sfg, std::string geom_type ) {
-  //   int n = get_sexp_length( sfg );
-  //   if( geom_type == "POINT" ) {
-  //     Rcpp::NumericVector nv = Rcpp::as< Rcpp::NumericVector >( sfg );
-  //     if (ISNAN( nv[0] ) ) {
-  //       return true;
-  //     }
-  //   } else if ( n == 0 ) {   // #nocov
-  //     return true;           // #nocov
-  //   }
-  //   return false;
-  // }
 
 } // namespace utils
 } // namespace geometries
