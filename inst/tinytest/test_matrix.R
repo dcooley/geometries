274e21e8b41fdc5662d2db1d6ec225cf6b50b827
SymbolixAU <dcooley@symbolix.com.au>
2020-07-29 14:55:35 +1000

    test

--- inst/tinytest/test_matrix.R
+++ inst/tinytest/test_matrix.R
@@ -14,7 +14,7 @@ expect_equal( geometries:::.test_to_geometry_matrix( df, c(1L,2L) ), unname( m[,
 expect_equal( geometries:::.test_to_geometry_matrix( df, c("x","y","z") ), unname( m ) )
 expect_equal( geometries:::.test_to_geometry_matrix( df, c("z","x") ), unname( m[, c(3,1)] ) )
 
-expect_equal( geometries:::.test_to_geometry_matrix( m, NULL ), unname( m ) )
+expect_equal( geometries:::.test_to_geometry_matrix( m, NULL ), m )  ## a named matrix keeps its names
 expect_equal( geometries:::.test_to_geometry_matrix( m, c(0L,1L,2L) ), unname( m ) )
 expect_equal( geometries:::.test_to_geometry_matrix( m, c(1L,2L) ), unname( m[, 2:3] ) )
 expect_equal( geometries:::.test_to_geometry_matrix( m, c("x","y","z") ), unname( m ) )
